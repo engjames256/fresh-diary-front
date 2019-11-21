2e677fdef02596352acbc1a8b6fd37f22cbba26e
Oyee James <engjames256@gmail.com>
2019-11-22 00:00:49 +0300

    Added form for registration

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,10 +1,90 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Register = ({ handleSubmit }) => {
   return (
-    <div>
-        <p>Code for registration here</p>
+    <div className="bg-white h-100-vh p-t-0">
+      <div className="page-loader">
+        <div className="spinner-border"></div>
+        <span>Loading ...</span>
+      </div>
+
+      <div className="p-b-50 d-block d-lg-none"></div>
+
+      <div className="container h-100-vh">
+        <div className="row align-items-md-center h-100-vh">
+          <div className="col-lg-6 d-none d-lg-block">
+            <img
+              className="img-fluid"
+              src="https://eagle.co.ug/wp-content/uploads/2018/02/fresh-family.png"
+              alt="..."
+            />
+          </div>
+          <div className="col-lg-4 offset-lg-1">
+            <p>
+              <center>Create a new account</center>
+            </p>
+            <form>
+              <div className="form-group mb-4">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  autofocus
+                  placeholder="Username"
+                />
+              </div>
+              <div className="form-group mb-4">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group mb-4">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group mb-4">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="custom-control custom-switch">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="customSwitch"
+                    checked=""
+                  />
+                  <label className="custom-control-label" for="customSwitch">
+                    I agree to the Terms and Privacy.
+                  </label>
+                </div>
+              </div>
+              <button className="btn btn-primary btn-lg btn-block btn-uppercase mb-4">
+                Create Account
+              </button>
+              <p className="text-right">
+                <NavLink to="/login" className="text-underline">
+                  Already a user?
+                </NavLink>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
