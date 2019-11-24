2ffc919c1fb8619d01ab857f3017b63f11f3f2f0
WalterNyeko <nyekowalter69@gmail.com>
2019-11-24 19:24:38 +0300

    fixed validations and showing products from firebase

--- src/containers/configs/utils.js
+++ src/containers/configs/utils.js
@@ -2,5 +2,5 @@ export const isFramelessPage = pathname => {
   return [`/`, `/login`, `/register`, `/forgotpassword`].includes(pathname);
 };
 
-// export const baseURL = "https://fresh-diary-back.herokuapp.com/";
-export const baseURL = "http://localhost:8080/";
+export const baseURL = "https://fresh-diary-back.herokuapp.com/";
+// export const baseURL = "http://localhost:8080/";
